f8c9f562dd4b9424e68a71df486a9e814d88b9a5
liupenghao03 <liupenghao03@kuaishou.com>
2026-04-07 20:35:01 +0800

    wget 移除 --continue

--- src/serverSetup.ts
+++ src/serverSetup.ts
@@ -458,7 +458,7 @@ if [[ ! -f $SERVER_SCRIPT ]]; then
     pushd $SERVER_DIR > /dev/null
 
     if [[ ! -z $(which wget) ]]; then
-        wget --tries=3 --timeout=10 --continue --no-verbose -O vscode-server.tar.gz $SERVER_DOWNLOAD_URL
+        wget --tries=3 --timeout=10 --no-verbose -O vscode-server.tar.gz $SERVER_DOWNLOAD_URL
     elif [[ ! -z $(which curl) ]]; then
         curl --retry 3 --connect-timeout 10 --location --show-error --silent --output vscode-server.tar.gz $SERVER_DOWNLOAD_URL
     else
